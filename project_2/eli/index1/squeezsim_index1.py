a570dccf131656b05f9ec5bee9cbfed63521bcac
Victor Miller <tfyvicmil@gmail.com>
2014-12-27 12:13:08 +0100

    Added squeezsim_index1.py

---
+++ project_2/eli/index1/squeezsim_index1.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 14 12:17:52 2014
+@author: Malin
+"""
+from  __future__  import division
+from  scipy       import *
+from  matplotlib.pyplot import *
+import numpy as np
+
+from assimulo.solvers import Dopri5
+from assimulo.solvers import CVode
+from assimulo.solvers import RungeKutta4
+from assimulo.problem import Explicit_Problem
+from assimulo.solvers import ExplicitEuler
+import squeezer_index1
+
+y0=squeezer_index1.init_squeezer() # Initial values
+t0 = 0 # Initial time
+
+squeezemodel = Explicit_Problem(squeezer_index1.squeezer_index1, y0,t0)
+
+sim = Dopri5(squeezemodel) # Create solver instance
+#sim.h = 0.01/100
+tf = .03 # End time for simulation
+sim.atol = 1e-7
+t,y=sim.simulate(tf)
+sim.plot(mask=7*[1]+7*[0])
+#plot(t,y[:,0:2])
+#plot(t, y[:,2:7])
+grid(1)
+axis([0, .03, -1.5, 1.5])
+xlabel('Time, t [s]')
+ylabel('Angle, [rad]')
